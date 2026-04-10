90cacfa36e720aff01afea0644009f1a6c457439
OooAldebaranooO <ifttt.jo@gmail.com>
2026-04-10 15:40:27 +0200

    Modifs CSS + trad

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,20 +4,47 @@ import {
   IonButton,
   IonContent,
   IonHeader,
+  IonSelect,
+  IonSelectOption,
   IonToolbar,
 } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { AppStateService } from '../../core/app-state.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, IonHeader, IonToolbar, IonContent, IonButton, TranslatePipe],
+  imports: [
+    CommonModule,
+    TranslatePipe,
+    IonHeader,
+    IonToolbar,
+    IonContent,
+    IonButton,
+    IonSelect,
+    IonSelectOption,
+  ],
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
   private router = inject(Router);
+  private translate = inject(TranslateService);
+  private state = inject(AppStateService);
+
+  currentLang = this.state.loadLanguage();
+  result = this.state.loadResult();
+
+  constructor() {
+    this.translate.use(this.currentLang);
+  }
+
+  changeLanguage(lang: string): void {
+    this.currentLang = lang;
+    this.translate.use(lang);
+    this.state.saveLanguage(lang);
+  }
 
   start(): void {
     this.router.navigateByUrl('/configurator');
