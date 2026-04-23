d11c81cece6f600bc473be275c87081019e964b6
OooAldebaranooO <ifttt.jo@gmail.com>
2026-04-23 17:21:34 +0200

    versionCode 9 - debug

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
+alert('main.ts chargé !');
+
 bootstrapApplication(AppComponent, appConfig).catch((err) => {
-  alert('Erreur : ' + JSON.stringify(err.message));
+  alert('Erreur bootstrap: ' + err.message);
 });
