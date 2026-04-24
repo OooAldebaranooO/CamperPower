d526fdc818a93a830175e8a877bc88b66dce7d1c
OooAldebaranooO <ifttt.jo@gmail.com>
2026-04-24 15:51:28 +0200

    versionCode 25 - debug

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,24 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.vechline.app',
+  appId: 'com.vechline_configurator',
   appName: 'Vechline Configurator',
   webDir: 'www',
   server: {
     allowNavigation: [
-      'www.tools-cmc-ea.fr'
+      'www.google.com',
+      '*.google.com',
+      'maps.googleapis.com',
+      'maps.gstatic.com',
+      'www.tools-cmc-ea.fr',
+      'nominatim.openstreetmap.org',
     ]
+  },
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: true,
+      showDuration: 2000,
+    }
   }
 };
 
