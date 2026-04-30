fd663cc737923e32a9694256ca285e4e5b71b66e
OooAldebaranooO <ifttt.jo@gmail.com>
2026-04-30 13:43:34 +0200

    Add iOS Platform

--- capacitor.podfile.js
+++ capacitor.podfile.js
@@ -8,11 +8,15 @@ module.exports = function (config) {
     async (config) => {
       const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       let contents = fs.readFileSync(podfilePath, 'utf-8');
+      console.log('=== PODFILE AVANT MODIFICATION ===');
+      console.log(contents);
       contents = contents.replace('use_modular_headers!', '');
       if (!contents.includes('use_frameworks!')) {
         contents = contents.replace('platform :ios', 'use_frameworks! :linkage => :static\nplatform :ios');
-        fs.writeFileSync(podfilePath, contents);
       }
+      console.log('=== PODFILE APRES MODIFICATION ===');
+      console.log(contents);
+      fs.writeFileSync(podfilePath, contents);
       return config;
     },
   ]);
