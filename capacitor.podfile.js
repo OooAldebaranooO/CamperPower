1e73d39af7d818562071d2228e913e9e92e90ee0
OooAldebaranooO <ifttt.jo@gmail.com>
2026-04-30 13:18:53 +0200

    Add iOS Platform

--- capacitor.podfile.js
+++ capacitor.podfile.js
@@ -1,8 +1,18 @@
-module.exports = function (api) {
-  api.modifyPodfile(({ contents }) => {
-    return contents.replace(
-      'platform :ios',
-      'use_modular_headers!\nplatform :ios'
-    );
-  });
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function (config) {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
+      let contents = fs.readFileSync(podfilePath, 'utf-8');
+      if (!contents.includes('use_modular_headers!')) {
+        contents = contents.replace('platform :ios', 'use_modular_headers!\nplatform :ios');
+        fs.writeFileSync(podfilePath, contents);
+      }
+      return config;
+    },
+  ]);
 };
