ea5fcad5be9d60bf6c69b06cc1d855b1bb851a74
OooAldebaranooO <ifttt.jo@gmail.com>
2026-05-13 10:43:17 +0200

    Push

--- src/app/core/push-notification.service.ts
+++ src/app/core/push-notification.service.ts
@@ -1,28 +1,14 @@
 import { Injectable } from '@angular/core';
-import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAaF0wJwPf8MnWhWIbUPtWbTaSFVs-HjSk",
-  authDomain: "vechline-configurator.firebaseapp.com",
-  projectId: "vechline-configurator",
-  storageBucket: "vechline-configurator.firebasestorage.app",
-  messagingSenderId: "1066915093085",
-  appId: "1:1066915093085:web:87c8dc4f80f47c51938bb4"
-};
-
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+import { FirebaseMessaging } from '@capacitor-firebase/messaging';
 
 @Injectable({ providedIn: 'root' })
 export class PushNotificationService {
 
   async requestPermission(): Promise<string | null> {
     try {
-      const token = await getToken(messaging, {
-        vapidKey: 'BA4QhsC1HA7kQfsVBX8Ii9qSmx7U71vUPO8dbs4lqmFt6sXVHh4ODD41kAQ7mFxzO6SwL-tc3VvrddQyjj4v7go'
-      });
-      console.log('Token FCM:', token);
+      await FirebaseMessaging.requestPermissions();
+      const { token } = await FirebaseMessaging.getToken();
+      console.log('Token Android:', token);
       return token;
     } catch (e) {
       console.error('Erreur permission:', e);
@@ -31,15 +17,8 @@ export class PushNotificationService {
   }
 
   listenToMessages() {
-    onMessage(messaging, (payload) => {
-        console.log('Notification reçue:', payload);
-
-        if (Notification.permission === 'granted') {
-        new Notification(payload.notification?.title ?? 'Nouveau message', {
-            body: payload.notification?.body,
-            icon: '/assets/icon/favicon.png'
-        });
-        }
+    FirebaseMessaging.addListener('notificationReceived', (notification) => {
+      console.log('Notification reçue:', notification);
     });
-    }
+  }
 }
